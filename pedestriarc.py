106c53b9a1ff153e0585bf052ef1ab31a1dfe784
Vincent Moulin <vincent.moulin@he-arc.ch>
2020-04-07 13:24:18 +0200

    added export in a json file

--- pedestriarc.py
+++ pedestriarc.py
@@ -6,9 +6,16 @@
 # Import
 from PIL import Image
 import glob
+import json
 
 # Functions
+
 def readData(foldername):
+    '''
+    Parameter : foldername = string who match a folder at the root of script
+    Return : image_list = list who contains file name of all .jpg file
+    Description : Creates a list who contains all file name ended by jpg and returns the list
+    '''
     image_list = []
     for filename in glob.glob(foldername + '/*.jpg'):
         im=Image.open(filename)
@@ -19,9 +26,17 @@ def readData(foldername):
 if __name__ == "__main__":
     foldername = "data"
     image_list = readData(foldername)
+    result = []
     for img in image_list:
-        print(img.filename)
+        containCrosswalk = True
+        roi = [15,30]
+        # Add dictionnary with information about picture on the result list
+        result.append({"file": img.filename, "crosswalk": containCrosswalk, "ROI": roi})
 
+    # Print the list result in a json format inside a file data.json
+    with open('data.json', 'w', encoding='utf-8') as f:
+        json.dump(result, f, ensure_ascii=False, indent=4)
+        print("data.json file has been successfuly saved!")
 
 
 
